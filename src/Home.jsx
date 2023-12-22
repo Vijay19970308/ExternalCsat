167154ac91903402be0fc558b72bc0b41964f740
Vijay Kumar <vijay.kumar3@indegene.com>
2023-12-22 16:19:32 +0530

    commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
 import { useOktaAuth } from '@okta/okta-react';
-import {
-    Button
-} from "antd";
 import Login from './Login';
 import Protected from './Protected';
 const Home = () => {
-    const { oktaAuth, authState } = useOktaAuth();
-    const history = useHistory();
+    const { authState } = useOktaAuth();
 
     if (!authState) {
         return <div>Loading ...</div>;
     }
-
-    const handleLogin = async () => history.push('/login');
-    const handleLogout = async () => oktaAuth.signOut();
 
     return (
         <div id="home">
