6fea2566cb5559ac92fca1723f85d0dc72ccb2f6
Vijay Kumar <vijay.kumar3@indegene.com>
2023-12-22 16:34:23 +0530

    commit

--- src/config.js
+++ src/config.js
@@ -3,14 +3,14 @@ const oktaConfig = {
         issuer: 'https://trial-1069723.okta.com',
         clientId: '0oa9v5ns4pUt1hWoX697',
         scopes: ['openid', 'profile', 'email'],
-        // redirectUri: `${window.location.origin}/login/callback`,
-        redirectUri: `https://65856b84cf62150008bda85f--chic-maamoul-6c3fcf.netlify.app/login/callback`
+        redirectUri: `${window.location.origin}/login/callback`,
+        // redirectUri: `https://65856b84cf62150008bda85f--chic-maamoul-6c3fcf.netlify.app/login/callback`
     },
     widget: {
         issuer: 'https://trial-1069723.okta.com',
         clientId: '0oa9v5ns4pUt1hWoX697',
-        // redirectUri: `${window.location.origin}/login/callback`,
-        redirectUri: `https://65856b84cf62150008bda85f--chic-maamoul-6c3fcf.netlify.app/login/callback`,
+        redirectUri: `${window.location.origin}/login/callback`,
+        // redirectUri: `https://65856b84cf62150008bda85f--chic-maamoul-6c3fcf.netlify.app/login/callback`,
 
         scopes: ['openid', 'profile', 'email'],
     }
